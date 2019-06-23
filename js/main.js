b09db94f62d43b5431ce05b0e998f84534ce3d2d
Tyler Harms <tyler.harms@gmail.com>
2019-06-22 23:18:01 -0500

    Publishing to gh-pages (publish.sh)

--- js/main.js
+++ js/main.js
@@ -5,7 +5,18 @@
 */
 
 (function ($) {
-
+    function viewport() {
+        var e = window,
+            a = 'inner';
+        if (!('innerWidth' in window)) {
+            a = 'client';
+            e = document.documentElement || document.body;
+        }
+        return {
+            width: e[a + 'Width'],
+            height: e[a + 'Height']
+        };
+    }
     // Breakpoints.
     skel.breakpoints({
         xlarge: '(max-width: 1680px)',
@@ -60,7 +71,21 @@
         if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
             $('#navPanel')
             .css('transition', 'none');
-
     });
+
+    var shareLinks = document.querySelectorAll('.share-link');
+    if (shareLinks.length) {
+        for(var i = 0; i <= shareLinks.length; i++) {
+            shareLinks[i].addEventListener('click', function(e){
+                e.preventDefault();
+                var $this = $(this);
+                var href = $this.attr('href');
+                var top = screen.width / 2 - 300;
+                var left = screen.height / 2 - 150;
+        
+                return viewport().width > 736 ? window.open(href, $this.attr('title'), 'toolbar=no, resizable=yes, location=no, width=600, height=300, menubar=no, status=no, scrollbars=no, directories=no, top=' + top + ', left=' + left) : window.open(href);
+            });
+        }
+    }
 
 })(jQuery);
