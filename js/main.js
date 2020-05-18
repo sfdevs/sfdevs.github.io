1df1cc12020c0fa09c11b93456cef910cc264223
Tyler Harms <tyler.harms@gmail.com>
2020-05-17 23:32:08 -0500

    Publishing to gh-pages (publish.sh)

--- js/main.js
+++ js/main.js
@@ -17,61 +17,6 @@
             height: e[a + 'Height']
         };
     }
-    // Breakpoints.
-    skel.breakpoints({
-        xlarge: '(max-width: 1680px)',
-        large: '(max-width: 1280px)',
-        medium: '(max-width: 980px)',
-        small: '(max-width: 736px)',
-        xsmall: '(max-width: 480px)'
-    });
-
-    $(function () {
-
-        var $window = $(window),
-            $body = $('body');
-
-        // Disable animations/transitions until the page has loaded.
-        $body.addClass('is-loading');
-
-        $window.on('load', function () {
-            window.setTimeout(function () {
-                $body.removeClass('is-loading');
-            }, 100);
-        });
-
-        // Prioritize "important" elements on medium.
-        skel.on('+medium -medium', function () {
-            $.prioritize(
-                '.important\\28 medium\\29',
-                skel.breakpoint('medium').active
-            );
-        });
-
-        // Off-Canvas Navigation.
-
-        // Navigation Panel.
-        $(
-                '<div id="navPanel">' +
-                $('#nav').html() +
-                '<a href="#navPanel" class="close"></a>' +
-                '</div>'
-            )
-            .appendTo($body)
-            .panel({
-                delay: 500,
-                hideOnClick: true,
-                hideOnSwipe: true,
-                resetScroll: true,
-                resetForms: true,
-                side: 'left'
-            });
-
-        // Fix: Remove transitions on WP<10 (poor/buggy performance).
-        if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-            $('#navPanel')
-            .css('transition', 'none');
-    });
 
     var shareLinks = document.querySelectorAll('.share-link');
     if (shareLinks.length) {
@@ -82,7 +27,7 @@
                 var href = $this.attr('href');
                 var top = screen.width / 2 - 300;
                 var left = screen.height / 2 - 150;
-        
+
                 return viewport().width > 736 ? window.open(href, $this.attr('title'), 'toolbar=no, resizable=yes, location=no, width=600, height=300, menubar=no, status=no, scrollbars=no, directories=no, top=' + top + ', left=' + left) : window.open(href);
             });
         }
